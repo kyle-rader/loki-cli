3743400809e48039f03ef5a5177c0b285d910abf
Kyle Rader <kylewrader@gmail.com>
2022-03-12 04:21:05 -0800

    Use single enum for CLI and return Results

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,51 @@
-use std::process::Command;
+use std::{ffi::OsStr, process::Command};
 
-use clap::{Parser, Subcommand};
+use clap::Parser;
 
 #[derive(Parser)]
 #[clap(version, about, author)]
-struct Cli {
-    #[clap(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
+enum Cli {
     /// Create a new branch and push it to origin.
     #[clap(visible_alias = "n")]
     New { name: Vec<String> },
 }
 
-fn main() {
+fn main() -> Result<(), String> {
     let cli = Cli::parse();
 
-    match &cli.command {
-        Commands::New { name } => new_branch(name),
+    match &cli {
+        Cli::New { name } => new_branch(name)?,
     }
+    Ok(())
 }
 
-fn new_branch(name: &Vec<String>) {
+fn new_branch(name: &Vec<String>) -> Result<(), String> {
     if name.len() == 0 {
-        eprintln!("We need some names!");
-        std::process::exit(1);
+        return Err(String::from("name cannot be empty."));
     }
 
     let name = name.join("-");
 
-    Command::new("git")
-        .arg("switch")
-        .arg("--create")
-        .arg(name.clone())
-        .status()
-        .expect("Failed to create branch!");
+    git(
+        "create new branch",
+        vec!["switch", "--create", name.as_str()],
+    )?;
 
-    Command::new("git")
-        .arg("push")
-        .arg("--set-upstream")
-        .arg("origin")
-        .arg(name)
-        .status()
-        .expect("Failed to push new branch!");
+    git(
+        "push to origin",
+        vec!["push", "--set-upstream", "origin", name.as_str()],
+    )?;
+
+    Ok(())
+}
+
+fn git<I, S>(name: &str, args: I) -> Result<(), String>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    if let Some(error) = Command::new("git").args(args).status().err() {
+        return Err(format!("{} failed to run: {}", name, error));
+    }
+    Ok(())
 }
