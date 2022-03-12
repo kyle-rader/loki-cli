ea466bb59dbf2865878627a431b6343c8b06749a
Kyle Rader <kylewrader@gmail.com>
2022-03-12 03:04:54 -0800

    Basic new command functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::process::Command;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(version, about, author)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create a new branch and push it to origin.
+    #[clap(visible_alias = "n")]
+    New { name: Vec<String> },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::New { name } => new_branch(name),
+    }
+}
+
+fn new_branch(name: &Vec<String>) {
+    if name.len() == 0 {
+        eprintln!("We need some names!");
+        std::process::exit(1);
+    }
+
+    let name = name.join("-");
+
+    Command::new("git")
+        .arg("switch")
+        .arg("--create")
+        .arg(name.clone())
+        .status()
+        .expect("Failed to create branch!");
+
+    Command::new("git")
+        .arg("push")
+        .arg("--set-upstream")
+        .arg("origin")
+        .arg(name)
+        .status()
+        .expect("Failed to push new branch!");
 }
