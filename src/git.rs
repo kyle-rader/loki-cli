cd0fcb8539f3c5bccd996d31d02f6891f632db88
Kyle Rader <kylewrader@gmail.com>
2022-03-13 14:08:18 -0700

    Implement push command

--- src/git.rs
+++ src/git.rs
@@ -1,17 +1,22 @@
-use std::{ffi::OsStr, process::Command};
+use std::{
+    ffi::OsStr,
+    process::{Command, Output},
+};
 
-pub fn git_command<I, S>(name: &str, args: I) -> Result<(), String>
+const GIT: &str = "git";
+
+pub fn git_command_status<I, S>(name: &str, args: I) -> Result<(), String>
 where
     I: IntoIterator<Item = S>,
     S: AsRef<OsStr>,
 {
-    if let Some(error) = Command::new("git").args(args).status().err() {
+    if let Some(error) = Command::new(GIT).args(args).status().err() {
         return Err(format!("{} failed to run: {}", name, error));
     }
     Ok(())
 }
 
-pub fn git_commands<C, I, S>(commands: C) -> Result<(), String>
+pub fn git_commands_status<C, I, S>(commands: C) -> Result<(), String>
 where
     C: IntoIterator<Item = (&'static str, I)>,
     I: IntoIterator<Item = S>,
@@ -19,6 +24,17 @@ where
 {
     commands
         .into_iter()
-        .map(|(name, cmd)| git_command(name, cmd))
+        .map(|(name, cmd)| git_command_status(name, cmd))
         .collect()
+}
+
+pub fn git_command_output<I, S>(name: &str, args: I) -> Result<Output, String>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    match Command::new(GIT).args(args).output() {
+        Ok(output) => Ok(output),
+        Err(err) => Err(format!("{} failed: {}", name, err)),
+    }
 }
