0891db6fbff834fa8d9ecec82dce2d2e647d7041
Kyle Rader <kylewrader@gmail.com>
2024-05-25 15:58:20 -0700

    format pruned msseage to line up up with git output

--- src/main.rs
+++ src/main.rs
@@ -213,7 +213,7 @@ fn prune(cmd: &str) -> Result<(), String> {
                 ) {
                     eprintln!("Failed to delete pruned branch {pruned_branch}: {err:?}")
                 } else {
-                    println!("💣 deleted local pruned branch {pruned_branch}");
+                    println!("💣      local   {pruned_branch} (pruned from remote)");
                 }
             }
         }
